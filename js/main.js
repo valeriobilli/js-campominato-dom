b1ec7447df7b75c757ff6173bf503aa8974f80f2
Valerio Billi <valerio.billi18@gmail.com>
2023-04-27 16:26:14 +0200

    copiata base esercizio precedente

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const grid = document.getElementById("grid");
+const play = document.getElementById("playBtn");
+play.addEventListener("click", startGame);
+
+var startGame = false;
+
+// funzione per generare le caselle in pagina
+function startGame() {
+    if (startGame == false) {
+        let nOfCell = document.getElementById("level").value;
+    let cellParSide = Math.sqrt(nOfCell)
+    let dimensione = `calc(100% / ${cellParSide})`
+    console.log("Gioco avviato")
+    
+    for (let i = 0; i < nOfCell; i++) {
+        const newSquare = document.createElement("div");
+        newSquare.classList.add("square");
+        newSquare.style.width = dimensione;
+        newSquare.style.height = dimensione;
+        grid.appendChild(newSquare);
+        newSquare.innerHTML = i+1; 
+
+        // funzione per colorare le caselle selezionate
+        newSquare.addEventListener("click", function(){
+            this.classList.toggle("selected")
+            console.log("Hai selezionato la casella numero", i+1)
+        })
+       
+    }
+
+    startGame = true;
+
+    } else {
+        console.log("Il gioco è già inizializzato");
+    }
+
+}
+
+let restart = document.getElementById("restartBtn");
+restart.addEventListener("click", refresh);
+
+// funnzione per refreshare e cominiciare una nuova partita
+function refresh(){
+    window.location.reload("Refresh")
+  }
